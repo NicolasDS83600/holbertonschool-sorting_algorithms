9b354b4072e93e03f73a1301524d262f5736a137
adib-commits <hdadib442@gmail.com>
2025-12-09 18:41:20 +0000

    Swap two integers

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,73 +1,80 @@
 #include "sort.h"
 
 /**
- * lomuto_partition - partitions the array using Lomuto scheme
+ * swap_int - Swap two integers
+ * @a: first int
+ * @b: second int
+ */
+void swap_int(int *a, int *b)
+{
+int tmp = *a;
+*a = *b;
+*b = tmp;
+}
+
+/**
+ * lomuto_partition - Lomuto partition scheme
  * @array: array to sort
- * @low: starting index
- * @high: ending index
- * @size: size of the array
- * Return: the pivot index
+ * @low: start index
+ * @high: end index (pivot)
+ * @size: array size (for printing)
+ * Return: pivot index after partition
  */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
 int pivot = array[high];
-int i = low;
-int j, temp;
+int i = low - 1, j;
 
 for (j = low; j < high; j++)
 {
 if (array[j] < pivot)
 {
+i++;
 if (i != j)
 {
-temp = array[i];
-array[i] = array[j];
-array[j] = temp;
+swap_int(&array[i], &array[j]);
 print_array(array, size);
 }
-i++;
 }
 }
 
-if (array[i] != array[high])
+if (i + 1 != high)
 {
-temp = array[i];
-array[i] = array[high];
-array[high] = temp;
+swap_int(&array[i + 1], &array[high]);
 print_array(array, size);
 }
 
-return (i);
+return (i + 1);
 }
 
 /**
- * quick_sort_recursive - sorts recursively using quick sort
+ * quicksort_recursive - Recursive helper
  * @array: array to sort
- * @low: starting index
- * @high: ending index
- * @size: size of the array
+ * @low: start index
+ * @high: end index
+ * @size: array size
  */
-void quick_sort_recursive(int *array, int low, int high, size_t size)
+void quicksort_recursive(int *array, int low, int high, size_t size)
 {
 int pivot_index;
 
 if (low < high)
 {
 pivot_index = lomuto_partition(array, low, high, size);
-quick_sort_recursive(array, low, pivot_index - 1, size);
-quick_sort_recursive(array, pivot_index + 1, high, size);
+quicksort_recursive(array, low, pivot_index - 1, size);
+quicksort_recursive(array, pivot_index + 1, high, size);
 }
 }
 
 /**
- * quick_sort - sorts array using quick sort algorithm
+ * quick_sort - Sorts an array using Quick sort (Lomuto)
  * @array: array to sort
- * @size: size of the array
+ * @size: size of array
  */
 void quick_sort(int *array, size_t size)
 {
-if (!array || size < 2)
+if (array == NULL || size < 2)
 return;
 
-quick_sort_recursive(array, 0, size - 1, size);
+quicksort_recursive(array, 0, size - 1, size);
 }
